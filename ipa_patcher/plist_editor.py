2877c92049b069002373f48b84ad921c5e5b4bb3
andreygirin144-netizen <andreygirin144@gmail.com>
2026-06-16 11:26:23 +0300

    Refactor plist_editor.py for code simplification

--- ipa_patcher/plist_editor.py
+++ ipa_patcher/plist_editor.py
@@ -1,35 +1,23 @@
 # -*- coding: utf-8 -*-
-"""Чтение, запись и редактирование Info.plist."""
-
-import plistlib
-import os
-
+import plistlib, os
 
 def load_plist(path):
-    """Загружает plist из файла."""
     with open(path, "rb") as f:
         return plistlib.load(f)
 
-
 def save_plist(data, path):
-    """Сохраняет plist в файл."""
     with open(path, "wb") as f:
         plistlib.dump(data, f)
 
-
 def patch_bundle_id(plist_path, old_id, new_id):
-    """Заменяет старый Bundle ID на новый в указанном plist (и в URL-типах, расширениях)."""
-    if not os.path.isfile(plist_path):
-        return
+    if not os.path.isfile(plist_path): return
     try:
         plist = load_plist(plist_path)
-    except Exception:
-        return
+    except: return
     changed = False
     if plist.get("CFBundleIdentifier") == old_id:
         plist["CFBundleIdentifier"] = new_id
         changed = True
-    # обновляем URL-схемы, если они содержат старый ID
     for url_type in plist.get("CFBundleURLTypes", []):
         if old_id in url_type.get("CFBundleURLName", ""):
             url_type["CFBundleURLName"] = url_type["CFBundleURLName"].replace(old_id, new_id)
@@ -39,7 +27,6 @@ def patch_bundle_id(plist_path, old_id, new_id):
             if old_id in scheme:
                 schemes[i] = scheme.replace(old_id, new_id)
                 changed = True
-    # специальные ключи для виджетов и расширений
     for key in ("WKAppBundleIdentifier", "WKCompanionAppBundleIdentifier"):
         if old_id in plist.get(key, ""):
             plist[key] = plist[key].replace(old_id, new_id)
@@ -47,9 +34,7 @@ def patch_bundle_id(plist_path, old_id, new_id):
     if changed:
         save_plist(plist, plist_path)
 
-
 def add_file_support(plist_data):
-    """Включает UIFileSharingEnabled и связанные ключи."""
     modified = False
     if not plist_data.get("UIFileSharingEnabled"):
         plist_data["UIFileSharingEnabled"] = True
