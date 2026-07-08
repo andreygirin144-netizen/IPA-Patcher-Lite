ad051652a0b5804539fae3d6e7b4f4a6026884e9
andreygirin144-netizen <andreygirin144@gmail.com>
2026-07-08 21:39:15 +0300

    Update constants.py

--- ipa_patcher/constants.py
+++ ipa_patcher/constants.py
@@ -16,3 +16,20 @@ SIGNATURE_FILES = frozenset({"embedded.mobileprovision", "CodeResources"})
 UNWANTED_DIRS = ["Library", "Applications", "DEBIAN"]
 
 MIN_HEADER_PADDING = 256
+
+class PatchConfig:
+    def __init__(self):
+        self.use_rpath = False
+        self.substrate_mode = 'auto'
+        self.substrate_source = None
+
+    def set_rpath(self, value):
+        self.use_rpath = bool(value)
+        from utils import log_message
+        log_message(f"RPATH mode set to: {self.use_rpath}", 'INFO')
+
+    def set_substrate_mode(self, mode):
+        if mode in ('auto', 'manual', 'none'):
+            self.substrate_mode = mode
+            from utils import log_message
+            log_message(f"Substrate mode set to: {self.substrate_mode}", 'INFO')
