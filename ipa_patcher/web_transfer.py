cba728941044ce2c79aa27b4f7deedd6d9c8d2d9
andreygirin144-netizen <andreygirin144@gmail.com>
2026-08-18 18:16:17 +0300

    Update web_transfer.py

--- ipa_patcher/web_transfer.py
+++ ipa_patcher/web_transfer.py
@@ -38,7 +38,7 @@ CONFIG = {
     'MAX_CONNECTIONS': 10,
 }
 
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 
 DOCS_DIR = os.path.expanduser('~/Documents')
 FILES_DIR = os.path.join(DOCS_DIR, 'IPA_Patcher_Files')
