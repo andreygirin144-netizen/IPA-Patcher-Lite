5476930bdbcab36ece2bac5fd1e525cae8f8406a
andreygirin144-netizen <andreygirin144@gmail.com>
2026-08-24 13:31:31 +0300

    Bump version from 1.1.3 to 1.1.4

--- ipa_patcher/web_transfer.py
+++ ipa_patcher/web_transfer.py
@@ -38,7 +38,7 @@ CONFIG = {
     'MAX_CONNECTIONS': 10,
 }
 
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 
 DOCS_DIR = os.path.expanduser('~/Documents')
 FILES_DIR = os.path.join(DOCS_DIR, 'IPA_Patcher_Files')
